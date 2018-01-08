b42264c133a288c1bb1742141c512b191d5bfa39
tlkeys <35232342+tlkeys@users.noreply.github.com>
2018-01-08 15:23:48 -0700

    did some stuff

---
+++ index.js
@@ -0,0 +1,7 @@
+// This file will house all of our common functions and objects that will be
+// used throughout the project, as well as allowing to enable & disable scripts
+
+//Configuration
+var scripts = {
+
+}
