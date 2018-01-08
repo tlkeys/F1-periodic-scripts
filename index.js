4ae528bf9d1e5d0b4b2cd6b33b1e1965b3829769
sploders101 <mariobuddy@gmail.com>
2018-01-08 16:06:40 -0700

    Created modular structure, ready for F1 login to continue development

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
 // used throughout the project, as well as allowing to enable & disable scripts
 
 //Configuration
-var scripts = {
+var scripts = [
+	{
+		name: "Phone number normalizer",
+		location: "./modules/phone",
+		enabled: true
+	},
+	{
+		name: "Capitalizer",
+		location: "./modules/capitalizer",
+		enabled: false
+	}
+];
 
+//Setup F1 API
+
+
+
+//END API setup
+
+//Require modules that will be used
+var path = require("path");
+
+var scriptResults = new Map();
+
+for(var i=0;i<scripts.length;i++) {
+	if(scripts[i].enabled) {
+		scriptResults.set( scripts[i].name , require(path.join(scripts[i].location,"index.js"))(f1api) )
+	}
 }
---
+++ modules/phone/index.js
@@ -0,0 +1,7 @@
+//This script will normalize phone records within the database to match format
+
+module.exports = function(f1api) {
+
+
+
+}
